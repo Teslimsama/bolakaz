d9040ce034f181cc23423e2864e8be12af7c1dfc
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2022-11-02 15:36:57 +0100

    profile edit system added add modified

--- shop.php
+++ shop.php
@@ -20,30 +20,30 @@ $pdo->close();
 
 ?>
 <?php
-                                // $conn = $pdo->open();
+// $conn = $pdo->open();
 
-                                // $slug = $_GET['product'];
+// $slug = $_GET['product'];
 
-                                // try {
+// try {
 
-                                //     $stmt = $conn->prepare("SELECT *, products.name AS prodname, category.name AS catname, products.id AS prodid FROM products LEFT JOIN category ON category.id=products.category_id WHERE slug = :slug");
-                                //     $stmt->execute(['slug' => $slug]);
-                                //     $product = $stmt->fetch();
-                                // } catch (PDOException $e) {
-                                //     echo "There is some problem in connection: " . $e->getMessage();
-                                // }
+//     $stmt = $conn->prepare("SELECT *, products.name AS prodname, category.name AS catname, products.id AS prodid FROM products LEFT JOIN category ON category.id=products.category_id WHERE slug = :slug");
+//     $stmt->execute(['slug' => $slug]);
+//     $product = $stmt->fetch();
+// } catch (PDOException $e) {
+//     echo "There is some problem in connection: " . $e->getMessage();
+// }
 
-                                // //page view
-                                // $now = date('Y-m-d');
-                                // if ($product['date_view'] == $now) {
-                                //     $stmt = $conn->prepare("UPDATE products SET counter=counter+1 WHERE id=:id");
-                                //     $stmt->execute(['id' => $product['prodid']]);
-                                // } else {
-                                //     $stmt = $conn->prepare("UPDATE products SET counter=1, date_view=:now WHERE id=:id");
-                                //     $stmt->execute(['id' => $product['prodid'], 'now' => $now]);
-                                // }
+// //page view
+// $now = date('Y-m-d');
+// if ($product['date_view'] == $now) {
+//     $stmt = $conn->prepare("UPDATE products SET counter=counter+1 WHERE id=:id");
+//     $stmt->execute(['id' => $product['prodid']]);
+// } else {
+//     $stmt = $conn->prepare("UPDATE products SET counter=1, date_view=:now WHERE id=:id");
+//     $stmt->execute(['id' => $product['prodid'], 'now' => $now]);
+// }
 
-                                ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -77,8 +77,8 @@ $pdo->close();
 
 <body>
 
-   <?php
-    include "includes/header.php";?>
+    <?php
+    include "includes/header.php"; ?>
     <?php include 'includes/navbar.php'; ?>
 
 
