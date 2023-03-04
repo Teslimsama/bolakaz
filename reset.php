489bf5eab6c5db80446888cb5f6b7ae675efb88c
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2023-03-04 17:51:21 +0100

    email token working

--- reset.php
+++ reset.php
@@ -3,60 +3,50 @@
 include 'session.php';
 
 if (isset($_POST['reset'])) {
-	$email = $_POST['email'];
+  $email = $_POST['email'];
 
-	$conn = $pdo->open();
+  $conn = $pdo->open();
 
-	$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email=:email");
-	$stmt->execute(['email' => $email]);
-	$row = $stmt->fetch();
+  $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM user WHERE email=:email");
+  $stmt->execute(['email' => $email]);
+  $row = $stmt->fetch();
 
-	if ($row['numrows'] > 0) {
-		//generate code
-		$set = '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		$code = substr(str_shuffle($set), 0, 15);
-		try {
-			$stmt = $conn->prepare("UPDATE users SET reset_code=:code WHERE id=:id");
-			$stmt->execute(['code' => $code, 'id' => $row['id']]);
+  if ($row['numrows'] > 0) {
+    //generate code
+    $set = '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $code = substr(str_shuffle($set), 0, 15);
+    try {
+      $stmt = $conn->prepare("UPDATE user SET reset_code=:code WHERE id=:id");
+      $stmt->execute(['code' => $code, 'id' => $row['id']]);
 
-			$message = "./email.php";
+      // email message
+      $to = $email;
+      $subject = 'Password Reset Link';
+      $message = "Dear ".$row['firstname'].",<br><br>Please click the link below to reset your password:<br><br>";
+      $message .= "<a href='http://localhost/bolakaz/password_reset.php?email=".$email."&code=".$code."'>Reset Password</a><br><br>";
+      $message .= "Thank you.<br>";
+      $message .= "Bolakaz Team<br>";
 
-			try {
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n";
+      $headers .= 'From: Bolakaz <noreply@unibooks.com.ng>' . "\r\n";
 
+      // send email
+      if (mail($to, $subject, $message, $headers)) {
+        $_SESSION['success'] = 'Password reset link sent';
+      } else {
+        $_SESSION['error'] = 'Message could not be sent. Please try again later.';
+      }
+    } catch (PDOException $e) {
+      $_SESSION['error'] = $e->getMessage();
+    }
+  } else {
+    $_SESSION['error'] = 'Email not found';
+  }
 
-				// $to = "bolajiteslim05@gmail.com"; // Change this email to your //
-				$subject = "$to";
-				$headers  = 'MIME-Version: 1.0' . "\r\n";
-				$headers .= 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n";
-				$From = "bolajiteslim05@gmail.com";
-				// Create email headers
-
-
-				// $header .= 'From: Bolakaz Enterprise <bolajiteslim05gmail.com>' . "\r\n";
-
-				// if (file_exists($tempfile)) {
-
-				// 	$message = file_get_contents($tempfile);
-				// 	$_SESSION['error'] = "sent";
-				// } else {
-				// 	die($_SESSION['error'] = "unable to locate file ");
-				// }
-
-				mail($email, $subject, $message, $header);
-				$_SESSION['success'] = 'Password reset link sent';
-			} catch (Exception $e) {
-				$_SESSION['error'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
-			}
-		} catch (PDOException $e) {
-			$_SESSION['error'] = $e->getMessage();
-		}
-	} else {
-		$_SESSION['error'] = 'Email not found';
-	}
-
-	$pdo->close();
+  $pdo->close();
 } else {
-	$_SESSION['error'] = 'Input email associated with account';
+  $_SESSION['error'] = 'Input email associated with account';
 }
 
-header('location: password_forgot.php');
+header('location: forgotten_password.php');
